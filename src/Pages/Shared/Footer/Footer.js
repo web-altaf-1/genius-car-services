885f0064faee16682ab6026cd1756963d8f514df
web-altaf-1 <web.altaf.1@gmail.com>
2022-04-12 20:45:42 +0600

    component setup and router setup

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p><small>&copy; Copyright 2022
+            </small></p>
+        </footer>
+    );
+};
+
+export default Footer;
