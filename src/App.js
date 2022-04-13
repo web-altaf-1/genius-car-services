dea2df30de631443656a00ff68adb4512e4ef5ab
web-altaf-1 <web.altaf.1@gmail.com>
2022-04-14 01:14:00 +0600

    raw css responsive design added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
-        <Route path="/" element={}></Route>
-        <Route path="/about" element={}></Route>
-        <Route path="/" element={}></Route>
-        <Route path="/" element={}></Route>
-        
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        {/* <Route path="/" element={}></Route>
+        <Route path="/" element={}></Route> */}
+
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
