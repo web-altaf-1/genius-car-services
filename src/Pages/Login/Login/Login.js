77f4a112bfde4a7f11f86c076dcc14905f99d3f9
web-altaf-1 <web.altaf.1@gmail.com>
2022-04-14 20:22:25 +0600

    login and register component complete

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const handleSubmitForm = event =>{
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password);
+    }
+
     return (
         <div className='container w-50 mx-auto'>
             <h2 className='text-center text-primary mt-2'>Please login !!</h2>
-            <Form>
+            <Form onSubmit={handleSubmitForm} className='shadow-lg p-5 rounded-lg'>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -17,7 +27,7 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Remember Me" />
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>Please Register</h2>
+        <div className='container w-50 mx-auto'>
+            <h2 className='text-center text-primary my-2'>Please Register</h2>
+            <Form className='shadow-lg p-5 rounded-lg'>
+                <Form.Group className="mb-3" controlId="">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control  type="text" placeholder="Enter Your Name" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="">
+                    <Form.Label>Your Email</Form.Label>
+                    <Form.Control  type="email" placeholder="Enter Your Email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Your Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter Your Password" required />
+                </Form.Group>
+                
+                <Form.Group className="mb-3" controlId="">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" placeholder="Confirm Password" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="I agree to Terms and condition" required/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="signup">
+                    <Form.Label>Already Have an Account ?? </Form.Label>
+                    <Link style={{ marginLeft: '5px' }} to='/login'>Please Login</Link>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
